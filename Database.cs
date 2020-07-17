3b5d2a9ab13b43913112506fa66807100060ea66
Erik <erik.mirai@outlook.com>
2020-07-17 14:20:39 +0300

    database generic added

---
+++ Database.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericOlegTask
+{
+    public class Database<T> where T : class, new()
+    {
+        private List<T> objects;
+
+        public Database()
+        {
+            this.objects = new List<T>();
+        }
+        public void DisplayAll()
+        {
+            foreach (var ob in this.objects)
+            {
+                Console.WriteLine(ob);
+            }
+        }
+        public void Add(T ob)
+        {
+            this.objects.Add(ob);
+        }
+    }
+}
